c98fb57d1180a0c3f47d9d7a316ba444ae418861
abbrevitory <riq@riseup.net>
2022-04-29 23:11:35 -0600

    removed space making shortened url unusable

--- shortener.go
+++ shortener.go
@@ -4,17 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 )
 
 func main() {
 	var input string
-	fmt.Println("Enter URL: ")
+	fmt.Println("Enter URL:")
 	fmt.Scanln(&input)
-	params := url.Values{}
-	params.Set(input, "") // encodes user input for get request from api
 
-	request, err := http.Get("https://api.shrtco.de/v2/shorten?url="+params.Encode())
+	request, err := http.Get("https://api.shrtco.de/v2/shorten?url="+input)
 	if err != nil {
 		fmt.Println(err)
 	}
